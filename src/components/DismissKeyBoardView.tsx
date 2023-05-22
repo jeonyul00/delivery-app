2c15f48eb14d379dc56c51a173c46f394c2c25d2
jeonyul <jeonyul00@gmail.com>
2023-05-22 10:55:48 +0900

    set : navigation

---
+++ src/components/DismissKeyBoardView.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  TouchableWithoutFeedback,
+  Keyboard,
+  StyleProp,
+  ViewStyle,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+
+const DismissKeyboardView: React.FC<
+  React.PropsWithChildren<{style: StyleProp<ViewStyle>}>
+> = ({children, ...props}) => (
+  <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+    <KeyboardAvoidingView
+      {...props}
+      style={props.style}
+      behavior={Platform.OS === 'android' ? undefined : 'padding'}>
+      {children}
+    </KeyboardAvoidingView>
+  </TouchableWithoutFeedback>
+);
+
+export default DismissKeyboardView;
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,14 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+
+const Settings = () => {
+  return (
+    <View>
+      <Text>Settings</Text>
+    </View>
+  );
+};
+
+export default Settings;
+
+const styles = StyleSheet.create({});
